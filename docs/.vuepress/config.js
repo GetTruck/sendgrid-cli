760bcc587f58ae8daaee0dd35936c10e165807c4
Shailen Naidoo <shailen@gettruck.co.za>
2019-06-08 19:36:30 +0200

    Setup vuepress docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: 'SendGrid CLI',
+  description: 'A fully-fledged CLI for the SendGrid API',
+  head: [
+    ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Palanquin&display=swap' }],
+  ],
+  themeConfig: {
+    
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,6 @@
+---
+home: true
+heroImage: https://static1.squarespace.com/static/597a08bc197aeafed2781380/t/5bca1c5be5e5f05d2c116971/1539972875789/sendgrid.png
+actionText: Get Started →
+actionLink: /guide/
+---
