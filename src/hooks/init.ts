1232cb621d6f2ba1497f9a0308dd7de79abff77d
Shailen Naidoo <shailen@gettruck.co.za>
2019-06-09 10:34:31 +0200

    Fixed an issue with init hook rewriting the data.json file

--- src/hooks/init.ts
+++ src/hooks/init.ts
@@ -2,7 +2,7 @@ import {Hook} from '@oclif/config'
 import fs from 'fs-extra';
 
 export const hook: Hook<'init'> = async function (options) {
-  if(!fs.pathExists(`${this.config.dataDir}/data.json`)) {
+  if(fs.pathExists(`${this.config.dataDir}/data.json`)) {
     return false;
   }
 
